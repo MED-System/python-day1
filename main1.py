b37c1c65ae419904a517eae62b6b09e6f5a476b5
MED-System <mohamed.edris1247@gmail.com>
2026-01-31 22:24:57 +0000

    First Day Code

---
+++ main1.py
@@ -0,0 +1,7 @@
+name = "George"
+age = "70"
+
+print("there was once old man called " + name)
+print("he was " + age + " years old")
+print("he really liked his " + name)
+print("but he didnt like being " + age)
